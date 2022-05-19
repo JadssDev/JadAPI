2dc99aee68687bce74c1bc6613acea2ea23a39f8
jadss <Jadss.pt.com.pt@gmail.com>
2022-05-19 17:34:53 +0100

    changed how the packet events are called, they should now be more consistent

--- API/src/main/java/dev/jadss/jadapi/listeners/JadEventHandler.java
+++ API/src/main/java/dev/jadss/jadapi/listeners/JadEventHandler.java
@@ -15,17 +15,30 @@ import org.bukkit.event.player.PlayerEvent;
 
 import java.util.ArrayList;
 
-public class JQuickEventsListener implements Listener {
+public class JadEventHandler implements Listener {
 
     @EventHandler //Quite worthless but why not?
     public void onEvent(Event event, EventPriority priority) {
-        if(JadAPI.getInstance().getDebug().doEventsDebug() && priority == EventPriority.LOWEST)
-            Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&3&lJadAPI &7>> &eAn &3&lEvent &ewas &b&lcalled &e-> &b" + event.getClass().getName() + " " + debugOptions(event)));
-        if(priority == EventPriority.LOWEST) JadAPI.getInstance().getInformationManager().addEventCall(event.getClass().getSimpleName());
-        for(JQuickEvent quickEvent : new ArrayList<>(JadAPI.getInstance().getInformationManager().getQuickEvents())) {
-            if (quickEvent.getEventType().isAssignableFrom(event.getClass()) && quickEvent.getPriority() == priority) {
-                quickEvent.run((quickEvent.getEventType().cast(event)));
+        try {
+            if(priority == EventPriority.LOWEST) {
+                JadAPI.getInstance().getInformationManager().addEventCall(event.getClass().getSimpleName());
+                if(JadAPI.getInstance().getDebug().doEventsDebug()) {
+                    Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&3&lJadAPI &7>> &eAn &3&lEvent &ewas &b&lcalled &e-> &b" + event.getClass().getName() + " " + debugOptions(event)));
+                }
             }
+
+            JadAPI.getInstance().getInformationManager().getQuickEvents().forEach(quickEvent -> {
+                if (quickEvent.getPriority() != priority)
+                    return;
+                if (quickEvent.getEventType() == JQuickEvent.ALL_EVENTS || quickEvent.getEventType().isInstance(event)) {
+                    //Is instance.
+                    if (quickEvent.check(event))
+                        quickEvent.run(event);
+                }
+            });
+        } catch (Exception ex) {
+            Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&3&lJadAPI &7>> &3&lJadEventHandler &eExploded and has stopped working! Please contact support."));
+            ex.printStackTrace();
         }
     }
 
