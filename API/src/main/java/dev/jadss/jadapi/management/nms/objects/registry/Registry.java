15517b768b0f7d1127c4bf9a1bb4d7abf6c917d8
jadss <Jadss.pt.com.pt@gmail.com>
2022-07-31 06:27:48 +0100

    fixed registries not working correctly

--- API/src/main/java/dev/jadss/jadapi/management/nms/objects/registry/Registry.java
+++ API/src/main/java/dev/jadss/jadapi/management/nms/objects/registry/Registry.java
@@ -1,24 +1,34 @@
 package dev.jadss.jadapi.management.nms.objects.registry;
 
 import dev.jadss.jadapi.bukkitImpl.enums.JVersion;
+import dev.jadss.jadapi.management.nms.NMS;
+import dev.jadss.jadapi.management.nms.NMSException;
 import dev.jadss.jadapi.management.nms.interfaces.NMSCopyable;
 import dev.jadss.jadapi.management.nms.interfaces.NMSManipulable;
 import dev.jadss.jadapi.management.nms.interfaces.NMSObject;
 import dev.jadss.jadapi.management.nms.objects.other.Key;
+import dev.jadss.jadapi.management.nms.objects.other.ResourceKey;
 import dev.jadss.jadapi.management.nms.objects.world.entities.tile.TileEntity;
 import dev.jadss.jadapi.management.nms.objects.world.entities.types.EntityTypesInstance;
-import dev.jadss.jadapi.utils.JReflection;
+import dev.jadss.jadapi.utils.reflection.JMappings;
+import dev.jadss.jadapi.utils.reflection.reflectors.JClassReflector;
+import dev.jadss.jadapi.utils.reflection.reflectors.JFieldReflector;
 
 import java.util.Map;
 
 public class Registry implements NMSObject, NMSManipulable, NMSCopyable {
 
-    public static final Class<?> I_REGISTRY = JReflection.getReflectionClass("net.minecraft. " + (JVersion.getServerVersion().isNewerOrEqual(JVersion.v1_17) ? "core" : "server." + JReflection.getNMSVersion()) + ".IRegistry");
+    public static final Class<?> I_REGISTRY = JClassReflector.getClass("net.minecraft. " + (JVersion.getServerVersion().isNewerOrEqual(JVersion.v1_17) ? "core" : "server." + NMS.getNMSVersion()) + ".IRegistry");
+
     //1.7 to 1.12
-    public static final Class<?> REGISTRY_SIMPLE = JReflection.getReflectionClass("net.minecraft.server." + JVersion.getServerVersion() + ".RegistrySimple");
+    public static final Class<?> REGISTRY_SIMPLE = JClassReflector.getClass("net.minecraft.server." + JVersion.getServerVersion() + ".RegistrySimple");
+
+    //1.14 to 1.19
+    public static final Class<?> WRITABLE_REGISTRY = JClassReflector.getClass("net.minecraft." + (JVersion.getServerVersion().isNewerOrEqual(JVersion.v1_17) ? "core" : "server." + NMS.getNMSVersion()) + ".IRegistryWritable");
+
     //All versions
-    public static final Class<?> REGISTRY_MATERIALS = JReflection.getReflectionClass("net.minecraft." + ((JVersion.getServerVersion().isNewerOrEqual(JVersion.v1_17)) ? "core" : "server." + JReflection.getNMSVersion()) + ".RegistryMaterials");
-    public static final Class<?> REGISTRY_BLOCKS = JReflection.getReflectionClass("net.minecraft." + ((JVersion.getServerVersion().isNewerOrEqual(JVersion.v1_17)) ? "core" : "server." + JReflection.getNMSVersion()) + ".RegistryBlocks");
+    public static final Class<?> REGISTRY_MATERIALS = JClassReflector.getClass("net.minecraft." + ((JVersion.getServerVersion().isNewerOrEqual(JVersion.v1_17)) ? "core" : "server." + NMS.getNMSVersion()) + ".RegistryMaterials");
+    public static final Class<?> REGISTRY_BLOCKS = JClassReflector.getClass("net.minecraft." + ((JVersion.getServerVersion().isNewerOrEqual(JVersion.v1_17)) ? "core" : "server." + NMS.getNMSVersion()) + ".RegistryBlocks");
 
     private final RegistryAdapter adapter;
 
@@ -32,17 +42,28 @@ public class Registry implements NMSObject, NMSManipulable, NMSCopyable {
     }
 
     public Object get(Key key) {
-        if (adapter.isOutdated())
+        if (adapter.isOutdated()) {
             return adapter.get(key.getKey());
-        else
-            return adapter.get(key);
+        } else {
+            return adapter.get(key.build());
+        }
     }
 
     public void set(Key key, Object value) {
-        if (adapter.isOutdated())
+        if (adapter.isOutdated()) {
             adapter.set(key.getKey(), value);
-        else
-            adapter.set(key, value);
+        } else {
+            if (JVersion.getServerVersion().isNewerOrEqual(JVersion.v1_16))
+                throw new NMSException("Cannot use this method due to version changes, please use Registry#set(ResourceKey, Object) instead!");
+            adapter.set(key.build(), value);
+        }
+    }
+
+    public void set(ResourceKey key, Object value) {
+        if (JVersion.getServerVersion().isLowerOrEqual(JVersion.v1_15))
+            throw new NMSException("Cannot use this method due to not being the correct version, please use Registry#set(Key, Object) instead!");
+
+        adapter.set(key.build(), value);
     }
 
     public RegistryAdapter getAdapter() {
@@ -64,39 +85,42 @@ public class Registry implements NMSObject, NMSManipulable, NMSCopyable {
         return new Registry(getter.apply(null));
     }
 
-    public enum Type {
-        MAIN((ignore) -> {
-            //This gets the list of the adapters in 1.13 (1.14 and above don't have this)
-            return new RegistryAdapter(JReflection.getFieldObject(Registry.I_REGISTRY, Registry.I_REGISTRY, null, (i) -> 0));
-        }),
+    public static final JMappings TILE_ENTITY_REGISTRY = JMappings.create(Registry.I_REGISTRY)
+            .add(JVersion.v1_13, "BLOCK_ENTITY_TYPE")
+            .add(JVersion.v1_16, "p")
+            .add(JVersion.v1_18, "n")
+            .finish();
 
+    public static final JMappings ENTITY_REGISTRY = JMappings.create(Registry.I_REGISTRY)
+            .add(JVersion.v1_13, "ENTITY_TYPE")
+            .add(JVersion.v1_16, "l")
+            .add(JVersion.v1_18, "j")
+            .finish();
+
+    public enum Type {
         TILE_ENTITY((ignore) -> {
             if (JVersion.getServerVersion().isNewerOrEqual(JVersion.v1_13)) {
                 //IRegistry class. (MinecraftKey, TileEntityTypes)
-                Registry registry = getRegistry(Type.MAIN);
-                Object object = registry.get(new Key("minecraft", "block_entity_type"));
-                return new RegistryAdapter(object);
+                return new RegistryAdapter(JFieldReflector.getObjectFromField(Registry.I_REGISTRY, TILE_ENTITY_REGISTRY.get(), null));
             } else if (JVersion.getServerVersion().isNewerOrEqual(JVersion.v1_11)) {
                 //Semi-newer IRegistry class. (String, TileEntity class)
-                return new RegistryAdapter(JReflection.getFieldObject(TileEntity.tileEntityClass, Registry.REGISTRY_MATERIALS, null, (i) -> i));
+                return new RegistryAdapter(JFieldReflector.getObjectFromUnspecificField(TileEntity.TILE_ENTITY, Registry.REGISTRY_MATERIALS, null));
             } else {
                 //Legacy.
-                return new RegistryAdapter(JReflection.getFieldObject(TileEntity.tileEntityClass, Map.class, null, (i) -> 0));
+                return new RegistryAdapter((Map<String, Object>) JFieldReflector.getObjectFromUnspecificField(TileEntity.TILE_ENTITY, Map.class, null));
             }
         }),
 
         ENTITY((ignore) -> {
             if (JVersion.getServerVersion().isNewerOrEqual(JVersion.v1_13)) {
                 //IRegistry class. (MinecraftKey, EntityTypes)
-                Registry registry = getRegistry(Type.MAIN);
-                Object object = registry.get(new Key("minecraft", "entity_type"));
-                return new RegistryAdapter(object);
+                return new RegistryAdapter(JFieldReflector.getObjectFromField(Registry.I_REGISTRY, ENTITY_REGISTRY.get(), null));
             } else if (JVersion.getServerVersion().isNewerOrEqual(JVersion.v1_11)) {
                 //Semi-newer IRegistry class. (String, Entity class)
-                return new RegistryAdapter(JReflection.getFieldObject(EntityTypesInstance.ENTITY_TYPE, Registry.REGISTRY_MATERIALS, null, (i) -> i));
+                return new RegistryAdapter(JFieldReflector.getObjectFromUnspecificField(EntityTypesInstance.ENTITY_TYPE, Registry.REGISTRY_MATERIALS, null));
             } else {
                 //Legacy.
-                return new RegistryAdapter(JReflection.getFieldObject(EntityTypesInstance.ENTITY_TYPE, Map.class, null, (i) -> 0));
+                return new RegistryAdapter((Map<String, Object>) JFieldReflector.getObjectFromUnspecificField(EntityTypesInstance.ENTITY_TYPE, Map.class, null));
             }
         });
 
--- API/src/main/java/dev/jadss/jadapi/management/nms/objects/registry/RegistryAdapter.java
+++ API/src/main/java/dev/jadss/jadapi/management/nms/objects/registry/RegistryAdapter.java
@@ -6,7 +6,10 @@ import dev.jadss.jadapi.management.nms.interfaces.NMSCopyable;
 import dev.jadss.jadapi.management.nms.interfaces.NMSManipulable;
 import dev.jadss.jadapi.management.nms.interfaces.NMSObject;
 import dev.jadss.jadapi.management.nms.objects.other.Key;
-import dev.jadss.jadapi.utils.JReflection;
+import dev.jadss.jadapi.management.nms.objects.other.ResourceKey;
+import dev.jadss.jadapi.utils.reflection.reflectors.JClassReflector;
+import dev.jadss.jadapi.utils.reflection.reflectors.JFieldReflector;
+import dev.jadss.jadapi.utils.reflection.reflectors.JMethodReflector;
 
 import java.util.Map;
 
@@ -15,6 +18,8 @@ public class RegistryAdapter implements NMSObject, NMSManipulable, NMSCopyable {
     private final boolean outdated;
     private final Map<Object, Object> registryMap;
     private final Object registryMaterials;
+
+    public static final Class<?> LIFE_CYCLE_CLASS = JClassReflector.getClass("com.mojang.serialization.Lifecycle");
 
     public RegistryAdapter(Map<Object, Object> registryMap) {
         this.outdated = true;
@@ -31,34 +36,58 @@ public class RegistryAdapter implements NMSObject, NMSManipulable, NMSCopyable {
         this.registryMaterials = registryMaterials;
     }
 
+    public ResourceKey getResourceKeyOfRegistry() {
+        if (JVersion.getServerVersion().isNewerOrEqual(JVersion.v1_16)) {
+            ResourceKey key = new ResourceKey();
+            key.parse(JFieldReflector.getObjectFromUnspecificField(Registry.I_REGISTRY, ResourceKey.RESOURCE_KEY_CLASS, registryMaterials));
+            return key;
+        } else {
+            throw new NMSException("This functionality is not available in 1.15 and below");
+        }
+    }
+
     public Object get(Object key) {
         if (outdated) {
             for (Object keyString : registryMap.keySet())
-                if (keyString.equals(key) || (keyString instanceof String && key instanceof String) && ((String) keyString).equalsIgnoreCase((String) key))
+                if (keyString.equals(key) || (key instanceof String && ((String) keyString).equalsIgnoreCase((String) key)))
                     return registryMap.get(keyString);
+
             return null;
         } else if (JVersion.getServerVersion().isLowerOrEqual(JVersion.v1_12)) {
             //get using an Object, returns object back.
-            return JReflection.executeMethod(Registry.REGISTRY_SIMPLE, new Class[] { Object.class }, registryMaterials, Object.class, (i) -> i, key);
+            return JMethodReflector.executeUnspecificMethod(Registry.REGISTRY_SIMPLE, new Class[]{Object.class}, Object.class, registryMaterials, new Object[]{key});
         } else {
             //get using a MinecraftKey, returns object back.
-            return JReflection.executeMethod(Registry.I_REGISTRY, new Class[] { Key.keyClass }, registryMaterials, Object.class, (i) -> i, ((Key) key).build());
+            if (JVersion.getServerVersion() == JVersion.v1_13) {
+                //Special case where they added 2 methods and so we have to go specific.
+                return JMethodReflector.executeMethod(Registry.I_REGISTRY, "get", registryMaterials, new Object[]{key});
+            } else {
+                //Every other case of an updated version.
+                return JMethodReflector.executeUnspecificMethod(Registry.I_REGISTRY, new Class[]{Key.KEY}, Object.class, registryMaterials, new Object[]{key});
+            }
         }
     }
 
     public void set(Object key, Object value) {
         if (outdated) {
-            throw new NMSException("Adding values to a Map is unsupported due to the limitations!");
+            throw new NMSException("Adding values to a Map is unsupported due to limitations!");
 //            if (registryMap.containsKey(key))
 //                throw new NMSException("You may not set a key that already exists in the registry.");
 //            else
 //                registryMap.put(key, value);
         } else if (JVersion.getServerVersion().isLowerOrEqual(JVersion.v1_12)) {
-            //set using an Object and Object.
-            JReflection.executeMethod(Registry.REGISTRY_SIMPLE, new Class[] { Object.class, Object.class }, registryMaterials, null, (i) -> 0, key, value);
+            //set using an Object and Object, return void.
+            JMethodReflector.executeUnspecificMethod(Registry.REGISTRY_SIMPLE, new Class[]{Object.class, Object.class}, void.class, registryMaterials, new Object[]{key, value});
+        } else if (JVersion.getServerVersion() == JVersion.v1_13) {
+            //set using a MinecraftKey and object, return the object.
+            JMethodReflector.executeUnspecificMethod(Registry.I_REGISTRY, new Class[]{Key.KEY, Object.class}, void.class, registryMaterials, new Object[]{key, value});
+        } else if (JVersion.getServerVersion().isLowerOrEqual(JVersion.v1_15)) {
+            //set using a MinecraftKey and object, return the object.
+            JMethodReflector.executeUnspecificMethod(Registry.WRITABLE_REGISTRY, new Class[]{Key.KEY, Object.class}, Object.class, registryMaterials, new Object[]{key, value});
         } else {
-            //set using a MinecraftKey and object.
-            JReflection.executeMethod(Registry.I_REGISTRY, new Class[] { Key.keyClass, Object.class }, registryMaterials, Object.class, (i) -> 0, ((Key) key).build());
+            //Set using a ResourceKey, object and Lifecycle, returns the object inputted.
+            JMethodReflector.executeUnspecificMethod(Registry.WRITABLE_REGISTRY, new Class[]{ResourceKey.RESOURCE_KEY_CLASS, Object.class, LIFE_CYCLE_CLASS}, registryMaterials, new Object[]{key, value,
+                    JMethodReflector.executeMethod(LIFE_CYCLE_CLASS, "stable", new Class[]{}, null)});
         }
     }
 
@@ -68,7 +97,8 @@ public class RegistryAdapter implements NMSObject, NMSManipulable, NMSCopyable {
 
     @Override
     public Object getHandle() {
-        return outdated ? registryMap : registryMaterials;
+        throw new NMSException("You may not get the handle of a Registry Object.");
+//        return outdated ? registryMap : registryMaterials;
     }
 
     @Override
