cde9a25794bd0d70a98442fe2bfec42a1b048cad
jadss <Jadss.pt.com.pt@gmail.com>
2022-07-31 06:13:39 +0100

    formatting and updated ItemStack api

--- API/src/main/java/dev/jadss/jadapi/bukkitImpl/entities/subEntities/JItem.java
+++ API/src/main/java/dev/jadss/jadapi/bukkitImpl/entities/subEntities/JItem.java
@@ -1,8 +1,8 @@
 package dev.jadss.jadapi.bukkitImpl.entities.subEntities;
 
 import dev.jadss.jadapi.bukkitImpl.entities.JEntity;
+import dev.jadss.jadapi.bukkitImpl.item.AbstractItemStack;
 import dev.jadss.jadapi.bukkitImpl.item.JItemStack;
-import dev.jadss.jadapi.exceptions.JException;
 import org.bukkit.entity.Item;
 
 /**
@@ -12,6 +12,7 @@ public class JItem extends JEntity {
 
     /**
      * Create a JItem from an existing Item.
+     *
      * @param item The Item
      */
     public JItem(Item item) {
@@ -20,24 +21,28 @@ public class JItem extends JEntity {
 
     /**
      * Get the item stack that item represents while dropped.
+     *
      * @return the JItemStack of it.
      */
-    public JItemStack getItemStack() { return new JItemStack(((Item) entity).getItemStack()); }
+    public AbstractItemStack<?> getItemStack() {
+        return new JItemStack(((Item) entity).getItemStack());
+    }
 
     /**
      * Set the ItemStack on the floor.
+     *
      * @param item the JItemStack to set the Item's ItemStack.
      * @return itself.
      */
-    public JItem setItemStack(JItemStack item) {
-        if(item == null) throw new JException(JException.Reason.VALUE_IS_NULL);
+    public JItem setItemStack(AbstractItemStack<?> item) {
+        ((Item) entity).setItemStack(item.getBukkitItem());
 
-        ((Item) entity).setItemStack(item.buildItemStack());
         return this;
     }
 
     /**
      * Set a delay on how many seconds the player has to wait to be able to pick the item up.
+     *
      * @param delay the delay in ticks.
      * @return itself.
      */
